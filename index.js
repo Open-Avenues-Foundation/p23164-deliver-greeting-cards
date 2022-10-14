0fdab5d613d4b33e554214947961e9afef1a087c
karankwatra <karankwatra@Karans-MacBook-Pro.local>
2022-10-14 15:55:35 -0700

    initial lob script

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const Lob = require('lob')('YOUR API KEY HERE');
+
+async function createPostcard () {
+  try {
+    const res = await Lob.postcards.create({
+      to: "adr_846654b8de3640a9",
+      front: "<h1> HI",
+      back: "<h1> HI"
+    })
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+(async () => {
+  createPostcard();
+})();
