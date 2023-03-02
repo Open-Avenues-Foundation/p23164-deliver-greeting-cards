cf7b980df66dc19ddf1733bfb16d044196a9d571
karankwatra <karankwatra@karans-mbp.lan>
2023-03-02 12:31:52 -0500

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pg = require('pg');
 const app = express();
 app.use(bodyParser.json()) // for parsing application/json
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const client = new pg.Client({
   user: process.env.DB_USER,
