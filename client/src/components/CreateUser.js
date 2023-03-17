a11092a4554c031eaa7217c1ea29ee74c20f3c09
Gabriela Sebastian <smolgaby27@gmail.com>
2023-03-17 18:28:39 -0400

    added create user client

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import axios from 'axios';
+import './CreateUser.css'
 
 export default class CreateUser extends React.Component {
+    state = {
+        name: '',
+        address_id: ''
+    }
+
+   
+    handleChange = event => {
+        this.setState({name : event.target.value});
+        console.log(event);
+        console.log(this.state);
+    }
+    handleChange1 = event => {
+        this.setState({address_id : event.target.value});
+        console.log(event);
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = {
+            name: this.state.name,
+            address_id : this.state.address_id
+        };
+
+        console.log(user);
+        axios.post(`https://deliver-greeting-cards.herokuapp.com/api/users`, {headers : {"content-type" : "application/JSON"}}, {user} )
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+
     render() {
         return (
-            <div className="home"> 
-                <h1>Create a User</h1>
+            <div className="form"> 
+           
+                <form onSubmit={this.handleSubmit}>
+                    <h1> Add a new user</h1>
+                    <label>
+                        Person Name:
+                        <input type="text" name="name" onChange={this.handleChange}/>
+                    </label>
+                
+                    <label>
+                        Address ID:
+                    <input type= "text" address_id="address_id" onChange={this.handleChange1}/>
+                    </label>
+    
+                    <button className = "btn" type="submit">Add</button>
+                </form>
             </div>
         );
     }
