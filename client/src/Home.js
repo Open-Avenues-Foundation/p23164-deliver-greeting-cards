7de1224afa5d188c222208a6dc6a055e8107fdf8
icy <icy@jupiter.local>
2023-11-21 15:14:32 -0800

    added postcard component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,20 +1,49 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Postcard from "./components/Postcard"
 
 const Home = () => {
-  // const [postcards, updatePostcards] = useState();
+  const [postcards, updatePostcards] = useState();
 
-  // useEffect(() => {
-  //   const cards = fetch('http://localhost:5001/api/postcards').then(data => console.log(data));
-  //   updatePostcards(cards);
-  // }, [])
+  useEffect(() => { //fetch to GET req
+    const fetchData = async () => {
+      try {
+        const cards = await fetch('http://localhost:5001/api/postcards');
+        const data = await cards.json(); // converts to json
+        updatePostcards(data.data); // Log data inside the async function
+        console.log(data.data);
+        return data;
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData(); // Invoke the fetchData function
+  }, []);
+ 
+ 
+
   return (
     <div className="home">
-      {/* {postcards.data.map(data => {
-        return <p>data.url</p>;
-      })} */}
-      <h1>sample content</h1>
+      {postcards ? postcards.map(data => {
+          return (
+            <Postcard 
+              data = {data}
+              key = {data.id}
+            /> 
+          )
+      }): "" } 
+   
+      <Postcard/>
     </div>
   );
 };
 
 export default Home;
+
+
+
+/* fixed how to fetch the data @ Home.js ... 
+i was so confused at first because but turns out i just need 
+to check for nulls / undefined. 
+ */
